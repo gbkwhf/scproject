c17b090d9ed16e0d8b154fda10753d8f7b405d1e
chen_rui <1270522168@qq.com>
2017-10-24 16:03:01 +0800

    update

---
+++ public/shopping_mall/js/config.js
@@ -0,0 +1,13 @@
+commonsUrl = 'http://gxdev.yxjk99.com/';
+versioninfos = '?os_type=android&version=9.0.0';
+TIMEOUT = 7000;
+//推送消息模板ID
+//测试
+template_id = "4QF2rUvMEGhsweb05YI2jqFRU3yRhap2UUuufVrAJU0";
+//公网
+//template_id = "rJgYIa_YzsLygDCqfRyvOKkzKY2Vp7o1qFpeBvsptYY";
+
+setCookie('commonsUrl',commonsUrl);
+setCookie('versioninfos',versioninfos);
+
+
