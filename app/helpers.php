452a3f59a79b0626efa9c7298ad6a963bdc5b958
xupan <532393819@qq.com>
2017-11-07 14:01:51 +0800

    'update'

--- app/helpers.php
+++ app/helpers.php
@@ -109,14 +109,15 @@ function  generatorOrderIdNew()
     $num = getRandomID(4,3);
 
     //return rand(1, 9) .$num.intval(($time[0] + $time[1]) * 100) . rand(0, 9); //做出如下修改，原因是因为中间出现了短横杠-,如：15601-20776480820
-    $str =  rand(1, 9) .$num.substr(intval(($time[0] + $time[1]) * 100),1) . rand(0, 9).substr($num,2);
-
-    if(strlen($str) == 16){
-        return $str;
-    }else{
-        $len = 16 - strlen($str);
-        return $str.substr(getRandomID(10,3),0,$len);
-    }
+    return rand(1, 9) .$num.substr(intval(($time[0] + $time[1]) * 100),1) . rand(0, 9).substr($num,2);
+//    $str =  rand(1, 9) .$num.substr(intval(($time[0] + $time[1]) * 100),1) . rand(0, 9).substr($num,2);
+//
+//    if(strlen($str) == 16){
+//        return $str;
+//    }else{
+//        $len = 16 - strlen($str);
+//        return $str.substr(getRandomID(10,3),0,$len);
+//    }
 
 
 }
