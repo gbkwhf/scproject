1b20f5786b9045f62cd9f9dc20dbcb3240030dc5
301chao <272128713@qq.com>
2017-11-16 17:06:03 +0800

    'wxconfig'

--- public/shopping_mall/invitation.php
+++ public/shopping_mall/invitation.php
@@ -42,6 +42,41 @@
 
     requesturl = commonsUrl+'shopping_mall/invitation.php';
     getWxConfig(requesturl);
+    wx.ready(function () {
+//        var tzurl = encodeURIComponent(commonsUrl+'/shopping_mall/userRegister.php?user_id='+user_id);
+        // 分享给朋友
+        wx.onMenuShareTimeline({
+            title: '双创共享商城', // 分享标题
+            desc: '双创共享商城免费注册送大礼', // 分享描述
+            link: "http://www.baidu.com", // 分享链接
+            imgUrl: commonsUrl+'/shopping_mall/images/logoimg.png', // 分享图标
+            type: '', // 分享类型,music、video或link，不填默认为link
+            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+            success: function () {
+                // 用户确认分享后执行的回调函数
+//                    layer.msg("分享成功！");
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+            }
+        });
+
+        wx.onMenuShareAppMessage({
+            title: '双创共享商城', // 分享标题
+            desc: '双创共享商城免费注册送大礼', // 分享描述
+            link: "http://www.baidu.com", // 分享链接
+            imgUrl: commonsUrl+'/shopping_mall/images/logoimg.png', // 分享图标
+            type: '', // 分享类型,music、video或link，不填默认为link
+            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+            success: function () {
+                // 用户确认分享后执行的回调函数
+//                    layer.msg("分享成功！");
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+            }
+        });
+    });
     $.ajax({
         type:'post',
         url:commonsUrl + 'api/gxsc/user/profile' + versioninfos,
@@ -50,42 +85,7 @@
 
             var user_id = data.result.user_id;//用户id
             console.log(user_id);
-            wx.ready(function () {
-                var tzurl = encodeURIComponent(commonsUrl+'/shopping_mall/userRegister.php?user_id='+user_id);
-                // 分享给朋友
-                wx.onMenuShareTimeline({
-                    title: '双创共享商城', // 分享标题
-                    desc: '双创共享商城免费注册送大礼', // 分享描述
-                    link: "http://www.baidu.com", // 分享链接
-                    imgUrl: commonsUrl+'/shopping_mall/images/logoimg.png', // 分享图标
-                    type: '', // 分享类型,music、video或link，不填默认为link
-                    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-                    success: function () {
-                        // 用户确认分享后执行的回调函数
-//                    layer.msg("分享成功！");
-                    },
-                    cancel: function () {
-                        // 用户取消分享后执行的回调函数
-                    }
-                });
 
-                wx.onMenuShareAppMessage({
-                    title: '双创共享商城', // 分享标题
-                    desc: '双创共享商城免费注册送大礼', // 分享描述
-                    link: "http://www.baidu.com", // 分享链接
-                    imgUrl: commonsUrl+'/shopping_mall/images/logoimg.png', // 分享图标
-                    type: '', // 分享类型,music、video或link，不填默认为link
-                    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-                    success: function () {
-                        // 用户确认分享后执行的回调函数
-//                    layer.msg("分享成功！");
-                    },
-                    cancel: function () {
-                        // 用户取消分享后执行的回调函数
-                    }
-                });
-
-            });
         }
     })
 
