32288a4fe78276d750550b91c67dd5175b9ec14b
freestronglei <514361421@qq.com>
2017-11-16 09:49:49 +0800

    registernew

--- public/shopping_mall/invitation.php
+++ public/shopping_mall/invitation.php
@@ -11,41 +11,69 @@
 </head>
 
 <body>
-
-	<div class="wrapper">
-		<div class="inviter">
-			<div class="photo">				
-				<img src="images/head-portrait.png" width="55"/>
-			</div>
-			<p>赵婉婷</p>
+	<div class="inviHead"><!--<img src="images/inviHead.png"/>--></div>
+	<div class="inviNav">
+		<div class="wrapNav">
+		<div class="inviTitle">推荐共享返利条件</div>
+		<div class="conNav">
+			<div>推荐人要求：必须在平台会员区产生消费并达到返利 资格的会员，方可推荐其他用户加入平台并注册。</div> 
+			<div>被推荐人必须在平台商城消费，且订单完成后（无退货）；如推荐人未在平台会员区消费，其被推荐人在会员区消费订单达到基数要求，推荐人将不能享受推荐共享返利。</div>
+			<div>推荐共享标准：每日平台（被推荐人平台交易的订单）总利润10%；</div>
+			<div>利润共享天数：永久（备注：只要被推荐人进行线上下单交易就可以享受推荐返利）。达到以上条件，平台会根据会员个人所推荐的总人数给予会员个人一定比例的推荐返利，推荐共享的金额每天根据财务数据统计，由系统自动返到会员的平台账户“可用余额”里。</div>
+			<h6> 备注：被推荐人消费达到自购消费基数，同样可享受会员自购利润共享返利。</h6>
 		</div>
-		<div class="inviter">
-			<div class="photo">				
-				<img src="images/head-portrait.png" width="55"/>
-			</div>
-			<p>赵婉婷</p>
 		</div>
-		<div class="inviter">
-			<div class="photo">				
-				<img src="images/head-portrait.png" width="55"/>
+		<div class="inviBtn1">邀请好友</div>
+		<div class="inviBtn2" onclick="location.href='invitationList.php'">我邀请的伙伴</div>
+		<div class="popBox" style="display: none;">
+			<div class="tiImg">
+				<div class="tiTitle">点击右上角按钮进行邀请</div>
 			</div>
-			<p>赵婉婷</p>
+			
 		</div>
 	</div>
-	
 </body>
 <script src="js/jquery.min.js"></script>
 <script src="js/layer/layer.js"></script>
 <script src="js/common.js"></script>
 <script src="js/config.js"></script>
-<script>
-	
-		
+<script type="text/javascript">
+$(function(){
+	$(".inviBtn1").click(function(){
+		$('.popBox').show()
+			$('.popBox').click(function() {
+				$('.popBox').hide()
+			})
+	})
+})
 	
 </script>
+<script>
+        	var user_id = $_GET['user_id'];
+        	console.log(user_id);
+        	
+          	wx.ready(function () {
+            // 分享给朋友
+            wx.onMenuShareAppMessage({
+                title: '双创共享商城', // 分享标题
+                desc: getCookie("is_member")+'双创共享商城免费注册送大礼', // 分享描述
+                link: commonUrl+'/wx/shopping_mall/userRegister.php?user_id='+user_id, // 分享链接
+                imgUrl: '', // 分享图标
+                type: '', // 分享类型,music、video或link，不填默认为link
+                dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                success: function () {
+                    // 用户确认分享后执行的回调函数
+                    layer.msg("分享成功！");
+                },
+                cancel: function () {
+                    // 用户取消分享后执行的回调函数
+                }
+            });
+        });
+
 <style type="text/css">
 	.layui-layer.layui-anim.layui-layer-page{
-		border-radius: 5px;
+		top: 20px;
 	} 
 </style>
 </html>
---
+++ public/shopping_mall/userRegister.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>用户注册</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=no">
+    	<meta name="apple-mobile-web-app-capable" content="yes"><meta name="format-detection" content="telephone=no" />
+   	 	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+    	<link rel="stylesheet" href="css/common.css">
+    	<link rel="stylesheet" href="css/userRegister.css">
+	</head>
+	<body>
+		<div class="usregHead"></div>
+		<div class="registerBox">
+			<div class="userMobil">
+				<input type="tel" class="inp inpmobile" maxlength="11" onkeyup="value=value.replace(/[^0-9.]/g,'') " id="inpmobile" placeholder="请输入手机号" />
+			</div>
+			<div class="inCode">
+				<input class="inp verify" id="verify" maxlength="6" type="text" placeholder="请输入验证码"/>
+                <div class="getCode" onkeyup="value=value.replace(/[^0-9.]/g,'') " onclick="getcode()">获取验证码</div>
+			</div>
+			<div class="registerBtn" onclick="reg()">注册</div>
+		</div>
+		<div class="regFooter">
+			<div class="activeBox">
+				<div class="inviTitle">活动内容</div>
+				<div class="nativeNav">在双创共享平台会员区通过现金或线上支付进行下单交易后， 其消费的金额，按照利润共享标准，给予相应的返利，由系统每天自动返还。其利润共享的金额显示在会员“可用余额”里，并可以直接提现。</div>
+			</div>
+			<div class="maImg">
+				<p><img src="images/ma.jpg"/></p>
+				<p style="padding-left: 69.5%;color: #ffffff;font-size: 12px;">双创共享</p>
+			</div>
+			<!--<div class="peopleImg"><img src="images/people.png"/></div>-->
+			
+			
+		</div>
+	</body>
+</html>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/common.js"></script>
+    <script type="text/javascript" src="js/config.js"></script>
+    <script type="text/javascript" src="js/layer/layer.js"></script>
+    <script>
+		
+        //获取验证码
+        function getcode(){
+            mobile = $('#inpmobile').val();
+            if(testTel(mobile)){
+                getCodeajax();
+            }else{
+                layer.msg('请输入正确的手机号');
+            }
+        }
+        function downtime(){
+            tt = 60;
+            $('.getCode').html(tt);
+            fortime = function timeDown(){
+                if(tt==0){
+                    clearInterval(myInterval);
+                    $('.getCode').html('重新获取');
+                    $('.getCode').attr('onclick','getcode()');
+                }else{
+                    tt--;
+                    $('.getCode').html(tt);
+                }
+            }
+            myInterval = setInterval('fortime()',1000);
+        }
+        //获取验证码
+        function getCodeajax(){
+            $('.getCode').attr('onclick','');
+            layer.load(2);
+            $.ajax({
+                url:commonsUrl+'api/gxsc/auth/send/user/sms'+versioninfos,
+                timeout:TIMEOUT,
+                method:'POST',
+                data:{
+                	service_type:5,
+                    un:mobile
+                },
+                success:function(data){
+                    layer.closeAll();
+                    if(data.code==1){
+                        downtime();
+                    }else{
+                        layer.msg(data.msg);
+                    }
+                },
+                type:'JSON'
+            });
+        }
+       //绑定
+        function reg(){
+            verify = $('#verify').val();
+            inpmobile = $('#inpmobile').val();
+            if(!verify||!inpmobile){
+                layer.msg('请填写手机验证码');
+            }else{
+            	verify = $('#verify').val();
+            	inpmobile = $('#inpmobile').val();
+                $.ajax({
+                    url:commonsUrl+'api/gxsc/bind/user/openId'+versioninfos,
+                    timeout:TIMEOUT,
+                    method:'POST',
+                    data:{
+                        'pin':verify,
+                        'openId': getCookie('openid'),
+                        'un':inpmobile,
+                        "invite_id":user_id
+                    },
+                    success:function(data){
+                        layer.closeAll();
+                        if(data.code==1){
+							layer.msg("注册成功");
+							
+							var lastpage = window.document.referrer;
+							setTimeout(function(){
+								if (/(iPhone|iPad|iPod)/i.test(navigator.userAgent)) {
+	                                window.location.href = window.document.referrer;
+	                            } else {
+	                                window.history.go(-1);	 
+	                        	}
+							},1000)
+                            
+                        }else{
+                            layer.msg(data.msg);
+                        }
+                    },
+                    type:'JSON'
+                });
+            }
+        }
+    </script>
