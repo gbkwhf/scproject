cd43a2eb0de33bb293112af7534762e3daad81e8
xupan <532393819@qq.com>
2017-11-16 14:33:07 +0800

    '更改配置文件'

--- app/Http/Controllers/Weixin/WeixinInfoController.php
+++ app/Http/Controllers/Weixin/WeixinInfoController.php
@@ -40,7 +40,8 @@ class WeixinInfoController  extends Controller{
     //获取公众号首页
     public function GetOfficalIndex(){
 
-        define("TOKEN", "shuangchuanggxzhengshi");
+        define("TOKEN", getenv('TOKEN'));
+//        define("TOKEN", "shuangchuanggxzhengshi");
 //        echo urlencode('http://gxdev.yxjk99.com/shopping_mall/index.php');die();
 
 
@@ -48,8 +49,14 @@ class WeixinInfoController  extends Controller{
 
         $appId = getenv('appId');
         $appSecret = getenv('appSecret');
+
+        $redirect_uri = urlencode(getenv('CALL_BACK_URL'));
+        $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$appId."&redirect_uri=".$redirect_uri."&response_type=code&scope=snsapi_base&state=123#wechat_redirect";
+
+
         $jssdk = new JSSDK($appId,$appSecret);
         $getAccessToken = $jssdk->getAccessToken();
+
 
        //data 菜单数据
         $data = '{
@@ -57,12 +64,12 @@ class WeixinInfoController  extends Controller{
                         {
                             "type": "view",
                             "name": "双创商城",
-                            "url": "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx97bfadf3a81d8206&redirect_uri=http%3A%2F%2Fgx.yxjk99.com%2Fshopping_mall%2Findex.php&response_type=code&scope=snsapi_base&state=123#wechat_redirect"
+                            "url": $url
                         }
                     ]
                 }';
 
-
+// "url": "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx97bfadf3a81d8206&redirect_uri=http%3A%2F%2Fgx.yxjk99.com%2Fshopping_mall%2Findex.php&response_type=code&scope=snsapi_base&state=123#wechat_redirect"
 
         if(isset($_GET['echostr'])){
             $wx->valid(); //如果发来了echostr则进行验证
