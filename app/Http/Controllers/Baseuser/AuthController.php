03d0ca5c7b10ca613b811440c25900baa386c1c2
xupan <532393819@qq.com>
2017-11-20 15:48:23 +0800

    'update'

--- app/Http/Controllers/Baseuser/AuthController.php
+++ app/Http/Controllers/Baseuser/AuthController.php
@@ -973,14 +973,43 @@ die('22222');
 
         $data = $jssdk->getUserInfo($openId);
 
+        $info = $this->setBaseInfo($data->headimgurl);
         print_r($data);die();
 
 
 
     }
 
+    private function setBaseInfo($url){
+
+        $return_content = $this->http_get_data($url);
+
+        echo $return_content;die();
+
+//        $filename = 'test.jpg';
+//        $fp= @fopen($filename,"a"); //将文件绑定到流 
+//        fwrite($fp,$return_content); //写入文件
 
 
+
+
+
+    }
+
+    private  function http_get_data($url) {
+
+        $ch = curl_init ();
+        curl_setopt ( $ch, CURLOPT_CUSTOMREQUEST, 'GET' );
+        curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, false );
+        curl_setopt ( $ch, CURLOPT_URL, $url );
+        ob_start ();
+        curl_exec ( $ch );
+        $return_content = ob_get_contents ();
+        ob_end_clean ();
+
+        $return_code = curl_getinfo ( $ch, CURLINFO_HTTP_CODE );
+        return $return_content;
+    }
     /**
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
