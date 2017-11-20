8d4a9dcbbd3aa6f11cced62da595c42b264d86ff
xupan <532393819@qq.com>
2017-11-20 15:22:09 +0800

    'update'

--- app/Http/Controllers/Weixin/jssdk.php
+++ app/Http/Controllers/Weixin/jssdk.php
@@ -89,7 +89,7 @@ class JSSDK{
 
         $accessToken = $this->getAccessToken();
         $url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$accessToken&openid=$openId&lang=zh_CN";
-        $res = json_decode($this->httpGetNew($url));
+        $res = json_decode($this->httpGet($url));
         return $res;
     }
 
@@ -127,22 +127,7 @@ class JSSDK{
     return $access_token;
   }
 
-    private function httpGetNew($url) {
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 500);
-        // 为保证第三方服务器与微信服务器之间数据传输的安全性，所有微信接口采用https方式调用，必须使用下面2行代码打开ssl安全校验。
-        // 如果在部署过程中代码在此处验证失败，请到 http://curl.haxx.se/ca/cacert.pem 下载新的证书判别文件。
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);
-//        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_URL, $url);
 
-        $res = curl_exec($curl);
-        curl_close($curl);
-
-        return $res;
-    }
 
   private function httpGet($url) {
     $curl = curl_init();
