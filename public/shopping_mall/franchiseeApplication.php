a92268c3bc1ae656a198e0824be8d112a46546ba
freestronglei <514361421@qq.com>
2017-12-04 17:14:39 +0800

    update

--- public/shopping_mall/franchiseeApplication.php
+++ public/shopping_mall/franchiseeApplication.php
@@ -52,49 +52,44 @@
 		</div>
 		<div class="confirmApply">确定申请</div>
 	</body>
+
 </html>
 <script src="js/jquery.min.js"></script>
 <script src="js/layer/layer.js"></script>
 <script src="js/common.js"></script>
 <script src="js/config.js"></script>
 <script type="text/javascript">
-//------------上传图片-------
+	//------------上传图片-------
 	$(function() {
-
-		var img = [];//创建一个空对象用来保存传入的图片
-		var AllowImgFileSize='1024';  //100kb
-			$("#inputs").change(function() {
-				var fil = this.files;
-				for(var i = 0; i < fil.length; i++) {
-	                var curr =$('#inputs')[i].files[0].size;
-	                 if(curr > AllowImgFileSize*1024)  {  //当图片大于80kb提示
-//	                 	layer.msg("图片文件大小超过限制 请上传小于"+AllowImgFileSize+"KB的文件");
-						layer.msg("图片文件大小超过限制 请上传小于1M的文件");
-	                 }else{
-	                 	reads(fil[i]);
-	                 	img.push($('#inputs')[i].files[0]);//将传入的图片push到空对象中
-	                 }
-					
+		var img = []; //创建一个空对象用来保存传入的图片
+		var AllowImgFileSize = '1024'; //1兆
+		$("#inputs").change(function() {
+			var fil = this.files;
+			for(var i = 0; i < fil.length; i++) {
+				var curr = $('#inputs')[i].files[0].size;
+				if(curr > AllowImgFileSize * 1024) { //当图片大于1兆提示
+					layer.msg("图片文件大小超过限制 请上传小于1M的文件");
+				} else {
+					reads(fil[i]);
+					img.push($('#inputs')[i].files[0]); //将传入的图片push到空对象中
 				}
-				if(img.length>=3){//判断图片的数量，数量不能超过3张
-			        $('.uploadDIv').hide();
-		         }else{
-		         	 $('.uploadDIv').show();
-		         }
-				console.log(img);
-			});	
-		
-		
+			}
+			if(img.length >= 3) { //判断图片的数量，数量不能超过3张
+				$('.uploadDIv').hide();
+			} else {
+				$('.uploadDIv').show();
+			}
+			console.log(img);
+		});
 		function reads(fil) {
 			var reader = new FileReader();
 			reader.readAsDataURL(fil);
-			
+
 			reader.onload = function() {
 				document.getElementById("uploadBox").innerHTML += "<div class='divImg' id='uploadImg'><img src='" + reader.result + "' class='imgBiMG'></div>";
 			}
 		}
 		$('.confirmApply').click(function() {
-			
 			//获取input框输入的值
 			inputForName = $('.inputForName').val();
 			inputForNum = $('.inputForNum').val();
@@ -106,21 +101,21 @@
 			console.log(inputProductName + 'ddddd');
 			console.log(inputCompany + 'ddddd');
 			console.log(inputAttributes + 'ddddd');
-			if(inputForName == "" || inputForName == undefined) {//对输入的值进行判断
+			if(inputForName == "" || inputForName == undefined) { //对输入的值进行判断
 				layer.msg("姓名不能为空");
 			} else if(!testTel(inputForNum) || inputForNum == '' || inputForNum == undefined || inputForNum == null) {
 				layer.msg("请输入正确的手机号码");
 			} else if(inputProductName == "" || inputProductName == undefined) {
 				layer.msg("产品名称不能为空");
 			} else {
-				var formData = new FormData();//创建一个空的formData对象用来保存变量参数
+				var formData = new FormData(); //创建一个空的formData对象用来保存变量参数
 				//				var arrhh= $('#inputs')[0].files[0]; //
 				//				console.log(arrhh);
 				//				img1 = $('.fileTest')[0].files[0];
 				//				img2 = $('.fileTest')[0].files[0];
 				//				img3 = $('.fileTest')[0].files[0];
 				//              console.log(img[0]);
-				formData.append("img_1", img[0]);//以键值对的形式将这些值保存到formData对象中
+				formData.append("img_1", img[0]); //以键值对的形式将这些值保存到formData对象中
 				formData.append("img_2", img[1]);
 				formData.append("img_3", img[2]);
 				formData.append("company_name", inputCompany);
@@ -128,20 +123,20 @@
 				formData.append("goods_name", inputProductName);
 				formData.append("mobile", inputForNum);
 				formData.append("name", inputForName);
-				 layer.load(2);
+				layer.load(2);
 				$.ajax({ //申请加盟
-					type: "post",//请求方式
+					type: "post", //请求方式
 					dataType: 'json',
-					url: commonsUrl + 'api/gxsc/joinsupplier' + versioninfos,//请求接口
-					data: formData,//请求参数（这里将参数都保存在formData对象中）
-					processData: false,//因为data值是FormData对象，不需要对数据做处理。
-					contentType: false,//默认为true,不设置Content-type请求头
+					url: commonsUrl + 'api/gxsc/joinsupplier' + versioninfos, //请求接口
+					data: formData, //请求参数（这里将参数都保存在formData对象中）
+					processData: false, //因为data值是FormData对象，不需要对数据做处理。
+					contentType: false, //默认为true,不设置Content-type请求头
 					success: function(data) {
 						console.log(data)
 						layer.closeAll();
 						if(data.code == 1) { //请求成功
 							//							layer.msg('上传成功');
-							location.href="subSuccess.php";
+							location.href = "subSuccess.php";
 						} else {
 							layer.msg(data.msg);
 						}
@@ -150,21 +145,5 @@
 				});
 			}
 		})
-
 	})
-</script>
-<script type="text/javascript">
-
-
-//	 var size = formatNum(img.fileSize / 1024,2);
-//    if(size > AllowImgFileSize)  {
-//         ErrMsgErrMsg = "\n\n图片文件大小超过限制 请上传小于"+AllowImgFileSize+"KB的文件，当前文件大小为"+size+"KB"; 
-//        alert(ErrMsgErrMsg); 
-//        return false;
-//      }else{
-//      	alert('ok!');
-//      return true;
-//      }
-
-
 </script>
