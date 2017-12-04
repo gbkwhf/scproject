acccf90bcad5b68a35363d8b93588e529888b798
freestronglei <514361421@qq.com>
2017-12-04 16:02:21 +0800

    update

--- public/shopping_mall/franchiseeApplication.php
+++ public/shopping_mall/franchiseeApplication.php
@@ -60,15 +60,22 @@
 <script type="text/javascript">
 //------------上传图片-------
 	$(function() {
+
 		var img = [];//创建一个空对象用来保存传入的图片
-		
+		var AllowImgFileSize='1024';  //1kb
 			$("#inputs").change(function() {
 				var fil = this.files;
 				for(var i = 0; i < fil.length; i++) {
-					reads(fil[i]);
-					img.push($('#inputs')[i].files[0]);//将传入的图片push到空对象中
+	                var curr =$('#inputs')[i].files[0].size;
+	                 if(curr > AllowImgFileSize*1024)  {  //当图片大于80kb提示
+	                 	layer.msg("图片文件大小超过限制 请上传小于"+AllowImgFileSize+"KB的文件);
+	                 }else{
+	                 	reads(fil[i]);
+	                 	img.push($('#inputs')[i].files[0]);//将传入的图片push到空对象中
+	                 }
+					
 				}
-				if(img.length>=3){
+				if(img.length>=3){//判断图片的数量，数量不能超过3张
 			        $('.uploadDIv').hide();
 		         }else{
 		         	 $('.uploadDIv').show();
@@ -80,6 +87,7 @@
 		function reads(fil) {
 			var reader = new FileReader();
 			reader.readAsDataURL(fil);
+			
 			reader.onload = function() {
 				document.getElementById("uploadBox").innerHTML += "<div class='divImg' id='uploadImg'><img src='" + reader.result + "' class='imgBiMG'></div>";
 			}
@@ -143,4 +151,19 @@
 		})
 
 	})
+</script>
+<script type="text/javascript">
+
+
+//	 var size = formatNum(img.fileSize / 1024,2);
+//    if(size > AllowImgFileSize)  {
+//         ErrMsgErrMsg = "\n\n图片文件大小超过限制 请上传小于"+AllowImgFileSize+"KB的文件，当前文件大小为"+size+"KB"; 
+//        alert(ErrMsgErrMsg); 
+//        return false;
+//      }else{
+//      	alert('ok!');
+//      return true;
+//      }
+
+
 </script>
