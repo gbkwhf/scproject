62b605a955abc03db77dce1a270b16808f829c9a
freestronglei <514361421@qq.com>
2018-01-25 09:27:43 +0800

    update

--- public/shopping_mall/newYearActive.php
+++ public/shopping_mall/newYearActive.php
@@ -12,19 +12,22 @@
 	<body>
 		<!--------放置背景图-->
 		<div class="wrapper">
-			<!--活动详情-->
-			<div class="activeDetails">活动详情</div>
-			<div class="avContent">
-				<p class="p1">即日起凡订购钻卡的用户，均享受 以下优惠,多买多送!</p>
-				<p class="p2">一张钻卡赠送两张银卡</p>
-				<p class="times">(活动时间：2018年1月17日-2018年2月12日)</p>
-			</div>
-			<!--活动说明-->
-			<div class="activeDescription">活动说明</div>
-			<div class="description">
-				<p class="destents">原：一张钻卡享受26张银卡返利 <br />现：一张钻卡+赠送两张银卡享受28张银卡的返利；</p>
-				<p class="explation">凡订购赠送的银卡只享受平台的返利，不能订购平台商品。不可与平台其他优惠活动同时享受。</p>
-				<p class="interpret">最终解释权归双创共享平台</p>
+			<div class="activeDetails">
+				<div class="active1">
+					<div class="activeName"><p class="scoll">钻卡</p></div>
+					<div class="activeContents">赠送红包2000元，并赠送银卡一张</div>
+				</div>
+				<div class="active2">
+					<div class="activeName"><p class="scoll2">白金卡</p></div>
+					<div class="activeContents2">赠送红包780元</div>
+				</div>
+				<div class="active3">
+					<div class="activeName"><p class="scoll3">金卡</p></div>
+					<div class="activeContents3">赠送红包388元</div>
+				</div>
+				<div class="activeMess">一次性办三张银卡，送88元现金</div>
+				<div class="tishi">特别提醒：2018年1月1日-1月24日（含24日）之前订钻卡的会员，在原促销活动（赠送两张银卡）的基础上，在给788元,以现金方式直接发给会员。</div>
+				<div class="times">活动时间:1月25日-2月12日</div>
 			</div>
 		</div>
 	</body>
