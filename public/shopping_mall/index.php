506f7e27ea2a495541614e4aab5dfa06c4eec555
freestronglei <514361421@qq.com>
2018-01-29 10:18:38 +0800

    update

--- public/shopping_mall/index.php
+++ public/shopping_mall/index.php
@@ -29,8 +29,8 @@
     <!------------弹出层------------>
     <div class="popBox" style="display: none;">
     	<div class="close"></div>
-    	<div class="favourable" onclick="location.href='newYearActive.php'">
-    		<img src="images/tanKBj.png"/>
+    	<div class="favourable" onclick="location.href='newYearsActive.php'">
+    		<img src="images/tanKBgg.png"/>
     	</div>
     </div>
     
