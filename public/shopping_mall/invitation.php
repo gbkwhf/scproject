eba62ff1262d8aa04cde2ef83f6c950e9cb0aea0
Administrator <1053175812@qq.com>
2018-05-17 10:26:00 +0800

    changesdata

--- public/shopping_mall/invitation.php
+++ public/shopping_mall/invitation.php
@@ -44,12 +44,14 @@
             if(invite_role==0){
                 location.href='myinvite.php'
             }else{
-                    wx.ready(function () {
+                   
+            }
+            wx.ready(function () {
     //        var tzurl = encodeURIComponent(commonsUrl+'/shopping_mall/userRegister.php?user_id='+user_id);
                     wx.onMenuShareAppMessage({
                         title: '双创共享商城', // 分享标题
                         desc: '双创共享商城免费注册送大礼', // 分享描述
-                        link: commonsUrl+'api/gxsc/invite/others/register?user_id='+user_id, // 分享链接
+                        link: commonsUrl + 'userRegister.php?user_id='+user_id, // 分享链接
                         imgUrl: commonsUrl+'/shopping_mall/images/logoimg.png', // 分享图标
                         type: '', // 分享类型,music、video或link，不填默认为link
                         dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
@@ -65,7 +67,7 @@
                     wx.onMenuShareTimeline({
                         title: '双创共享商城', // 分享标题
                         desc: '双创共享商城免费注册送大礼', // 分享描述
-                        link: commonsUrl+'api/gxsc/invite/others/register?user_id='+user_id, // 分享链接
+                        link: commonsUrl + 'userRegister.php?user_id='+user_id, // 分享链接
                         imgUrl: commonsUrl+'/shopping_mall/images/logoimg.png', // 分享图标
                         type: '', // 分享类型,music、video或link，不填默认为link
                         dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
@@ -78,7 +80,6 @@
                         }
                     });
                 });
-            }
         }
     })
 
