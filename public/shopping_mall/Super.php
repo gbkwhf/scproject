781c8aee721d815bf8c56b7e2206466f49be2512
jiafei <1053175812@qq.com>
2018-10-30 16:25:38 +0800

    商品分类

--- public/shopping_mall/Super.php
+++ public/shopping_mall/Super.php
@@ -16,16 +16,12 @@
 <div class="clarity"></div>
 <div id="body">
     <div style="position: fixed;top: 0;left: 0;background: #fff;width: 100%;z-index: 99;">
-        <div class="wrapper wrapper02" id="wrapper02">
-            <div class="scroller">
-                <ul class="clearfix">
-                    <script type="text/html" id="navList">
-                        <li>
-                            <a href="javascript:void(0)" id="{{goods_second_id}}">{{goods_second_name}}</a>
-                        </li>
-                    </script>
-                </ul>
-            </div>
+        <div style="height: 30px;overflow: hidden;position: relative;border-bottom: 1px solid #ccc;">
+            <ul class="tab-head">
+                <script type="text/html" id="navList">
+                    <li class="tab-head-item" id="{{goods_second_id}}">{{goods_second_name}}</li>
+                </script>
+            </ul>
             <img src="images/bot.png" id="show" alt="">
         </div>
     </div>
@@ -58,35 +54,29 @@
 <script type="text/javascript" src="js/common.js"></script>
 <script type="text/javascript" src="js/config.js"></script>
 <script src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/navbar/flexible.js"></script>
-<script type="text/javascript" src="js/navbar/iscroll.js"></script>
-<script type="text/javascript" src="js/navbar/navbarscroll.js"></script>
 <script type="text/javascript">
     $(function () {
-        alert('123')
-        alert(getCookie('openid'))
-        alert(getCookie($_GET['store_first_id']))
-        // let page = 1;
-        // let store_second_id = '';
-        // if ($_GET["store_first_id"] == 1) {
-        //     $("title").text("商超")
-        // } else if ($_GET["store_first_id"] == 2) {
-        //     $("title").text("精品馆")
-        // } else {
-        //     $("title").text("土特产")
-        // }
-        //
-        // $(".clarity").css("height", $(this).height() + "px")
-        //
-        // $("#show").click(function () {
-        //     $(".float").show()
-        //     $(".clarity").show()
-        // })
-        //
-        // $("#hide").click(function () {
-        //     $(".float").hide()
-        //     $(".clarity").hide()
-        // })
+        let page = 1;
+        let store_second_id = '';
+        if ($_GET["store_first_id"] == 1) {
+            $("title").text("商超")
+        } else if ($_GET["store_first_id"] == 2) {
+            $("title").text("精品馆")
+        } else {
+            $("title").text("土特产")
+        }
+
+        $(".clarity").css("height", $(this).height() + "px")
+
+        $("#show").click(function () {
+            $(".float").show()
+            $(".clarity").show()
+        })
+
+        $("#hide").click(function () {
+            $(".float").hide()
+            $(".clarity").hide()
+        })
 
         // // 导航分类
         $.ajax({
@@ -105,42 +95,45 @@
                         $("#show").hide()
                     }
                     store_second_id = data[0].store_second_id
+
                     for (let val of data) {
                         let temp = $("#navList").html()
                         temp = temp.replace("{{goods_second_name}}", val.store_second_name).replace("{{goods_second_id}}", val.store_second_id)
-                        $(".clearfix").append(temp)
+                        $(".tab-head").append(temp)
                         $(".float ul").append("<li id=" + val.store_second_id + '>' + val.store_second_name + "</li>")
                     }
+                    $(".tab-head li").eq(0).addClass("select").siblings().removeClass("select");
+                    shop(store_second_id, page);
+                    $(".tab-head li").click(function () {
+                        console.log($(this).attr('id'))
+                        store_second_id = $(this).attr('id')
+                        let index = $(this).index()
+                        $(this).addClass("select").siblings().removeClass("select");
+                        page = 1
+                        $(".tem li").remove()
 
-        //             $('.wrapper').navbarscroll();
-        //             shop(store_second_id, page);
-        //             $(".clearfix li").click(function (e) {
-        //                 store_second_id = e.target.id
-        //                 let index = $(this).index()
-        //                 $(this).addClass("select").siblings().removeClass("select");
-        //                 // $(".msg li").hide().eq(index).show()
-        //                 page = 1
-        //                 $(".tem li").remove()
-        //
-        //                 shop(store_second_id, page)
-        //             })
-        //
-        //
-        //             $(".float li").click(function () {
-        //                 let store_second_id = $(this).attr("id")
-        //                 let index = $(this).index()
-        //                 let nums = -parseInt(index) / 0.02
-        //                 $(".scroller").attr("style", "width: " + data.length * 98 + "px;transition-timing-function: cubic-bezier(0.1, 0.57, 0.1, 1);transition-duration: 0ms;transform: translate(" + nums + "px, 0px) translateZ(0px);")
-        //                 $(".tem li").remove()
-        //                 shop(store_second_id, page)
-        //                 $(".float").hide()
-        //                 $(".clarity").hide()
-        //                 $(".clearfix li").each(function () {
-        //                     if (index == $(this).index()) {
-        //                         $(this).addClass("cur").siblings().removeClass("cur")
-        //                     }
-        //                 })
-        //             })
+                        shop(store_second_id, page)
+                    })
+
+
+                    $(".float li").click(function () {
+                        let store_second_id = $(this).attr("id")
+                        let index = $(this).index()
+                        $(".tem li").remove()
+                        shop(store_second_id, page)
+                        $(".float").hide()
+                        $(".clarity").hide()
+                        $('.tab-head li').each(function () {
+                            if(index == $(this).index()){
+                                var moveX = $(this).position().left + $(this).parent().scrollLeft();
+                                var pageX = document.documentElement.clientWidth;//设备的宽度
+                                var blockWidth = $(this).width();
+                                var left = moveX - (pageX / 2) + (blockWidth / 2);
+                                $(".tab-head").animate({scrollLeft: left}, 400);
+                                $(this).addClass("select").siblings().removeClass("select");
+                            }
+                        })
+                    })
                 } catch (e) {
                     console.log(e)
                     layer.msg(res.msg);
@@ -148,48 +141,48 @@
 
             }
         })
-        //
-        //
-        // $(this).scroll(function () {
-        //     var viewHeight = $(this).height();//可见高度
-        //     var contentHeight = $("#body").get(0).scrollHeight;//内容高度
-        //     var scrollHeight = $(this).scrollTop();//滚动高度
-        //     if ((contentHeight - viewHeight) / scrollHeight <= 1) {
-        //         page++
-        //         shop(store_second_id, page, show)
-        //     }
-        // })
-        //
-        //
-        // // // 门店
-        // function shop(id, page, show) {
-        //     $.ajax({
-        //         type: "POST",
-        //         dataType: "json",
-        //         url: commonsUrl + 'api/gxsc/get/store/list' + versioninfos,
-        //         data: {
-        //             ss: getCookie('openid'),
-        //             store_second_id: id,
-        //             page: page
-        //         },
-        //         success: (res) => {
-        //             console.log(res)
-        //             let data = res.result
-        //             if (data.length == 0) {
-        //                 if (!show) {
-        //                     $(".show").show()
-        //                 }
-        //             } else {
-        //                 $(".show").hide()
-        //             }
-        //             for (let val of data) {
-        //                 let temp = $("#commentList").html()
-        //                 temp = temp.replace("{{logo}}", val.logo).replace("{{store_name}}", val.store_name).replace("{{store_id}}", val.store_id).replace("{{name}}", val.store_name)
-        //                 $(".tem").append(temp)
-        //             }
-        //         }
-        //     })
-        // }
+
+
+        $(this).scroll(function () {
+            var viewHeight = $(this).height();//可见高度
+            var contentHeight = $("#body").get(0).scrollHeight;//内容高度
+            var scrollHeight = $(this).scrollTop();//滚动高度
+            if ((contentHeight - viewHeight) / scrollHeight <= 1) {
+                page++
+                shop(store_second_id, page, show)
+            }
+        })
+
+
+        // // 门店
+        function shop(id, page, show) {
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: commonsUrl + 'api/gxsc/get/store/list' + versioninfos,
+                data: {
+                    ss: getCookie('openid'),
+                    store_second_id: id,
+                    page: page
+                },
+                success: (res) => {
+                    console.log(res)
+                    let data = res.result
+                    if (data.length == 0) {
+                        if (!show) {
+                            $(".show").show()
+                        }
+                    } else {
+                        $(".show").hide()
+                    }
+                    for (let val of data) {
+                        let temp = $("#commentList").html()
+                        temp = temp.replace("{{logo}}", val.logo).replace("{{store_name}}", val.store_name).replace("{{store_id}}", val.store_id).replace("{{name}}", val.store_name)
+                        $(".tem").append(temp)
+                    }
+                }
+            })
+        }
 
     })
 </script>
