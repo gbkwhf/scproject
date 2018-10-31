9a8304550b44c44f9c5be0641d675cdb49bac836
jiafei <1053175812@qq.com>
2018-10-31 09:49:12 +0800

    商品分类

--- public/shopping_mall/js/super.js
+++ public/shopping_mall/js/super.js
@@ -37,44 +37,45 @@ $(function () {
                 }
                 store_second_id = data[0].store_second_id
 
-                for (var val in data) {
+                for (var i in data) {
+                    console.log(data[i])
                     var temp = $("#navList").html()
-                    temp = temp.replace("{{goods_second_name}}", data[val].store_second_name).replace("{{goods_second_id}}", data[val].store_second_id)
+                    temp = temp.replace("{{goods_second_name}}", data[i].store_second_name).replace("{{goods_second_id}}", data[i].store_second_id)
                     $(".tab-head").append(temp)
-                    $(".float ul").append("<li id=" + data[val].store_second_id + '>' + data[val].store_second_name + "</li>")
+                    $(".float ul").append("<li id=" + data[i].store_second_id + '>' + data[i].store_second_name + "</li>")
                 }
                 $(".tab-head li").eq(0).addClass("select").siblings().removeClass("select");
-                // shop(store_second_id, page);
-                // $(".tab-head li").click(function () {
-                //     console.log($(this).attr('id'))
-                //     store_second_id = $(this).attr('id')
-                //     var index = $(this).index()
-                //     $(this).addClass("select").siblings().removeClass("select");
-                //     page = 1
-                //     $(".tem li").remove()
-                //
-                //     shop(store_second_id, page)
-                // })
-                //
-                //
-                // $(".float li").click(function () {
-                //     var store_second_id = $(this).attr("id")
-                //     var index = $(this).index()
-                //     $(".tem li").remove()
-                //     shop(store_second_id, page)
-                //     $(".float").hide()
-                //     $(".clarity").hide()
-                //     $('.tab-head li').each(function () {
-                //         if (index == $(this).index()) {
-                //             var moveX = $(this).position().left + $(this).parent().scrollLeft();
-                //             var pageX = document.documentElement.clientWidth;//设备的宽度
-                //             var blockWidth = $(this).width();
-                //             var left = moveX - (pageX / 2) + (blockWidth / 2);
-                //             $(".tab-head").animate({scrollLeft: left}, 400);
-                //             $(this).addClass("select").siblings().removeClass("select");
-                //         }
-                //     })
-                // })
+                shop(store_second_id, page);
+                $(".tab-head li").click(function () {
+                    console.log($(this).attr('id'))
+                    store_second_id = $(this).attr('id')
+                    var index = $(this).index()
+                    $(this).addClass("select").siblings().removeClass("select");
+                    page = 1
+                    $(".tem li").remove()
+
+                    shop(store_second_id, page)
+                })
+
+
+                $(".float li").click(function () {
+                    var store_second_id = $(this).attr("id")
+                    var index = $(this).index()
+                    $(".tem li").remove()
+                    shop(store_second_id, page)
+                    $(".float").hide()
+                    $(".clarity").hide()
+                    $('.tab-head li').each(function () {
+                        if (index == $(this).index()) {
+                            var moveX = $(this).position().left + $(this).parent().scrollLeft();
+                            var pageX = document.documentElement.clientWidth;//设备的宽度
+                            var blockWidth = $(this).width();
+                            var left = moveX - (pageX / 2) + (blockWidth / 2);
+                            $(".tab-head").animate({scrollLeft: left}, 400);
+                            $(this).addClass("select").siblings().removeClass("select");
+                        }
+                    })
+                })
             } catch (e) {
                 console.log(e);
             }
@@ -86,45 +87,45 @@ $(function () {
     })
 
 
-    // $(this).scroll(function () {
-    //     var viewHeight = $(this).height();//可见高度
-    //     var contentHeight = $("#body").get(0).scrollHeight;//内容高度
-    //     var scrollHeight = $(this).scrollTop();//滚动高度
-    //     if ((contentHeight - viewHeight) / scrollHeight <= 1) {
-    //         page++
-    //         shop(store_second_id, page, show)
-    //     }
-    // })
+    $(this).scroll(function () {
+        var viewHeight = $(this).height();//可见高度
+        var contentHeight = $("#body").get(0).scrollHeight;//内容高度
+        var scrollHeight = $(this).scrollTop();//滚动高度
+        if ((contentHeight - viewHeight) / scrollHeight <= 1) {
+            page++
+            shop(store_second_id, page, show)
+        }
+    })
 
 
     // 门店
-    // function shop(id, page, show) {
-    //     $.ajax({
-    //         type: "POST",
-    //         dataType: "json",
-    //         url: commonsUrl + 'api/gxsc/get/store/list' + versioninfos,
-    //         data: {
-    //             ss: getCookie('openid'),
-    //             store_second_id: id,
-    //             page: page
-    //         },
-    //         success: function(res) {
-    //             console.log(res)
-    //             var data = res.result
-    //             if (data.length == 0) {
-    //                 if (!show) {
-    //                     $(".show").show()
-    //                 }
-    //             } else {
-    //                 $(".show").hide()
-    //             }
-    //             for (var val in data) {
-    //                 var temp = $("#commentList").html()
-    //                 temp = temp.replace("{{logo}}", data[val].logo).replace("{{store_name}}", data[val].store_name).replace("{{store_id}}", data[val].store_id).replace("{{name}}", data[val].store_name)
-    //                 $(".tem").append(temp)
-    //             }
-    //         }
-    //     })
-    // }
+    function shop(id, page, show) {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: commonsUrl + 'api/gxsc/get/store/list' + versioninfos,
+            data: {
+                ss: getCookie('openid'),
+                store_second_id: id,
+                page: page
+            },
+            success: function(res) {
+                console.log(res)
+                var data = res.result
+                if (data.length == 0) {
+                    if (!show) {
+                        $(".show").show()
+                    }
+                } else {
+                    $(".show").hide()
+                }
+                for (var val in data) {
+                    var temp = $("#commentList").html()
+                    temp = temp.replace("{{logo}}", data[val].logo).replace("{{store_name}}", data[val].store_name).replace("{{store_id}}", data[val].store_id).replace("{{name}}", data[val].store_name)
+                    $(".tem").append(temp)
+                }
+            }
+        })
+    }
 
 })
